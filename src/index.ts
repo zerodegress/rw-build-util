daae9784f8078bbcdfa4f864940c07436e64530f
zerodegress <zerodegress@outlook.com>
2023-02-01 19:03:10 +0800

    clear most of the legacy

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export * as rwtoml from "./data/toml";
-export { RwToml, Section as TomlSection, SectionGroup as TomlSectionGroup, Value as TomlValue, Scalar as TomlScalar, Vector as TomlVector } from './data/toml';
-export * as rwini from "./data/ini";
-export { RwIni, Section as IniSection, Value as IniValue } from "./data/ini";
-export * as rwbuilder from "./builder";
-export * as tomlbuilder from "./builder/toml";
-export * as opt from "./util/optional";
-export * as result from "./util/result";
-export * as modpath from "./util/path";
--- src/util/functional.ts
+++
@@ -1,9 +0,0 @@
-import { Optional } from "./optional";
-
-export interface Converter<T> {
-    (x: T): T;
-}
-
-export interface MapFilter<T> {
-    (x: T): Optional<T>;
-}
